e2cfe4d287c9780d66869f5b83c85a1e34aeb56a
= <=>
2021-06-16 14:33:23 +0700

    New update

--- FPTWebApplication/Controllers/TraineesController.cs
+++ FPTWebApplication/Controllers/TraineesController.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using FPTWebApplication.Models;
 
 namespace FPTWebApplication.Controllers
 {
     public class TraineesController : Controller
     {
+        private Trainee _trainee;
+        public TraineesController()
+        {
+            _trainee = new Trainee()
+            {
+                Id = 1,
+                FullName = "Nguyen Van Loc",
+                BDay = DateTime.Now,
+                Course = "Application Deverlopment"
+            };
+        }
         // GET: Trainees
         public ActionResult Index()
         {
-            return View();
+            return View(_trainee);
         }
     }
 }
