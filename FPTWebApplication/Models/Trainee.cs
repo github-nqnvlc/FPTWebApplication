efe46a058cd29ee30ad6aca850912ffd054b3cba
= <=>
2021-06-16 14:24:23 +0700

    Trainee

---
+++ FPTWebApplication/Models/Trainee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FPTWebApplication.Models
+{
+    public class Trainee
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public DateTime BDay { get; set; }
+        public string Course { get; set; }
+
+    }
+}
