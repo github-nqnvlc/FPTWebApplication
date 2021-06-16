26e12bc384b413673ee47606e6c4479b826ab690
= <=>
2021-06-16 15:15:48 +0700

    Display Mutiple Trainee items

--- FPTWebApplication/Controllers/TraineesController.cs
+++ FPTWebApplication/Controllers/TraineesController.cs
@@ -9,22 +9,31 @@ namespace FPTWebApplication.Controllers
 {
     public class TraineesController : Controller
     {
-        private Trainee _trainee;
+        private List<Trainee> _trainees;   
         public TraineesController()
         {
-            _trainee = new Trainee()
+            _trainees = new List<Trainee>();
+            _trainees.Add(new Trainee()
             {
                 Id = 1,
                 FullName = "Nguyen Van Loc",
                 Gender = "Male",
                 BDay = DateTime.Now,
                 Course = "Application Deverlopment"
-            };
+            });
+            _trainees.Add(new Trainee()
+            {
+                Id = 2,
+                FullName = "Tran Quan Duy",
+                Gender = "Male",
+                BDay = DateTime.Now,
+                Course = "Application Deverlopment"
+            });
         }
         // GET: Trainees
         public ActionResult Index()
         {
-            return View(_trainee);
+            return View(_trainees);
         }
     }
 }
