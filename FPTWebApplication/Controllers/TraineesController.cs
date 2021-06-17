0f3817afda985e386bc112899ce4776c91f928d8
= <=>
2021-06-17 20:56:04 +0700

    implement index and detail action with context

--- FPTWebApplication/Controllers/TraineesController.cs
+++ FPTWebApplication/Controllers/TraineesController.cs
@@ -9,36 +9,24 @@ namespace FPTWebApplication.Controllers
 {
     public class TraineesController : Controller
     {
+        private ApplicationDbContext _context;
         private List<Trainee> _trainees;
         public TraineesController()
         {
-            _trainees = new List<Trainee>();
-            _trainees.Add(new Trainee()
-            {
-                Id = 1,
-                FullName = "Nguyen Van Loc",
-                Gender = "Male",
-                BDay = DateTime.Now,
-                Course = "Application Deverlopment"
-            });
-            _trainees.Add(new Trainee()
-            {
-                Id = 2,
-                FullName = "Tran Quan Duy",
-                Gender = "Male",
-                BDay = DateTime.Now,
-                Course = "Application Deverlopment"
-            });
+
+            _context = new ApplicationDbContext();
         }
         // GET: Trainees
         public ActionResult Index()
         {
-            return View(_trainees);
+            var trainees = _context.Trainees.ToList();
+            return View(trainees);
         }
 
         public ActionResult Details(int id)
         {
-            var trainee = _trainees.SingleOrDefault(t => t.Id == id);
+            if (id == null) return HttpNotFound();
+            var trainee = _context.Trainees.SingleOrDefault(t => t.Id == id);
             if (trainee == null)
                 return HttpNotFound();
             return View(trainee);
